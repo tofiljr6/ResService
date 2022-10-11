c6a1a443b6cf5bb14613a5ef8f2cbba0db8849b0
tofiljr6 <mateusz.tofil52@gmail.com>
2022-10-11 19:50:20 +0200

    Initial Commit

---
+++ ResService/ResServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResServiceApp.swift
+//  ResService
+//
+//  Created by Mateusz Tofil on 11/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ResServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
